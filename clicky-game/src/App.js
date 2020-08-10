ea52f91b3de0672947bcebfecd325bc716d1dda4
Aidan Sweeny <apswee22@colby.edu>
2020-08-10 18:01:09 -0400

    Added initial files

---
+++ clicky-game/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Game from "./components/Game";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Game} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
