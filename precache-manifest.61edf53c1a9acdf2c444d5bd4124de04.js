4e77f058f493c6cf548c2e4e03cadabeccbd0c7b
Aidan Sweeny <apswee22@colby.edu>
2020-08-11 19:41:55 -0400

    Updates

---
+++ precache-manifest.61edf53c1a9acdf2c444d5bd4124de04.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b21125821d64c2e07c127fca79616be",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f652c710b9169259d469",
+    "url": "/clicky-game/static/js/2.6ef17e12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clicky-game/static/js/2.6ef17e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a461de60de03f268a4a",
+    "url": "/clicky-game/static/js/main.02bab987.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  },
+  {
+    "revision": "b3bceb38bf4ef6819dcce5440591a1ed",
+    "url": "/clicky-game/static/media/aang.b3bceb38.jpg"
+  },
+  {
+    "revision": "517542caf62981eef3b315f0728bdc42",
+    "url": "/clicky-game/static/media/azula.517542ca.jpg"
+  },
+  {
+    "revision": "ce63ddcbd5256d4dc0f52ea033ed7fc8",
+    "url": "/clicky-game/static/media/banner.ce63ddcb.jpg"
+  },
+  {
+    "revision": "7a7d3848c4de7da0a7bd00a60a1570b9",
+    "url": "/clicky-game/static/media/iroh.7a7d3848.jpg"
+  },
+  {
+    "revision": "8ae1709a645fb86cc7c4ef75c1a70043",
+    "url": "/clicky-game/static/media/jeong-jeong.8ae1709a.jpg"
+  },
+  {
+    "revision": "6d3c74d222da88ead38fa262fcbdd6ba",
+    "url": "/clicky-game/static/media/momo.6d3c74d2.jpg"
+  },
+  {
+    "revision": "e0f014e0e2feaacde48978c014b6e9b1",
+    "url": "/clicky-game/static/media/sokka.e0f014e0.jpg"
+  },
+  {
+    "revision": "b5a7ac624c7088458299b2ed898c2729",
+    "url": "/clicky-game/static/media/zuko.b5a7ac62.jpg"
+  }
+]);
